1080c194fc5c58f4aa9a90f25d1c02d8cc1dd961
Marc Morin <mmarcjr@hotmail.com>
2016-12-16 21:36:59 -0500

    zz

--- www/js/index.js
+++ www/js/index.js
@@ -24,14 +24,19 @@ function onDeviceReady()
 function setContentPage1(from)
 {
 	$('#p1ContentTile').html($('#p1ContentTile').html() + 'From: ' + from);
+	
 	var screen = $.mobile.getScreenHeight(); 
 	$('#p1ContentTile').html($('#p1ContentTile').html() + ' - screen: ' + screen);
+	
 	var header = $("#p1Header").outerHeight();
 	$('#p1ContentTile').html($('#p1ContentTile').html() + ' - p1Header: ' + header);
+	
 	var footer = $("#p1footer").outerHeight();
 	$('#p1ContentTile').html($('#p1ContentTile').html() + ' - p1footer: ' + footer);
+	
 	var contentCurrent = $("#p1Content").outerHeight() - $("#p1Content").height();
 	$('#p1ContentTile').html($('#p1ContentTile').html() + ' - contentCurrent: ' + contentCurrent);
+	
 	var content = screen - header - footer - contentCurrent;
 	$('#p1ContentTile').html($('#p1ContentTile').html() + ' - content: ' + content);
 	$('#p1ContentTile').html($('#p1ContentTile').html() + ' - Total: ' + (header + footer + contentCurrent + content) + '<br>');
